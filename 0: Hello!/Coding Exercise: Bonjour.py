1877d0a200f15a2cba4b1b29c7f8e9e62ab323c4
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-31 19:12:47 +0530

    Create Coding Exercise: Bonjour.py

---
+++ 0: Hello!/Coding Exercise: Bonjour.py
@@ -0,0 +1,8 @@
+"""
+Write a program which prints
+  " Bonjour, tout le monde! "
+  
+(Hint: if you get stuck, copy the first program above and then edit it.)
+"""
+
+print("Bonjour, tout le monde!")
