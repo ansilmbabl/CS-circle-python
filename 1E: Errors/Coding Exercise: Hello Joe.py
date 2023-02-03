e8aa0e9f1d77ad3ec929fd660a946d1b6388a491
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-03 20:56:00 +0530

    Create Coding Exercise: Hello Joe.py

---
+++ 1E: Errors/Coding Exercise: Hello Joe.py
@@ -0,0 +1,11 @@
+"""
+Coding Exercise: Hello Joe
+
+Fix the run-time error in the following program, so that it prints out Hello on the first line and Joe on the second line. You can change at most two characters.
+"""
+
+
+print("Hello")
+username = "Joe" 
+print(username)
+
