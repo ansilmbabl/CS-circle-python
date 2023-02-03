7d1c6d781347a788b3df60f1ea5a2c9e52b03e93
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-03 20:49:26 +0530

    Create Coding Exercise: Heads, Shoulders, Knees and Toes.py

---
+++ 1: Variables/Coding Exercise: Heads, Shoulders, Knees and Toes.py
@@ -0,0 +1,15 @@
+"""
+Coding Exercise: Heads, Shoulders, Knees and Toes
+
+Write a code fragment (a short part of a Python program) to count heads, shoulders, knees, and toes at a party. 
+The grader will automatically define a variable people for you, counting the number of people at the party. 
+Your code must define four variables, one called heads, one called shoulders, one called knees, and one called toes, equal to the number of heads, 
+shoulders, knees, and toes in total at the party. Your program does not need to print any output. 
+The grader will select a new random value for people each time your code runs.
+
+"""
+
+heads = 1* people
+shoulders = 2* people
+knees = 2* people
+toes = 10* people
