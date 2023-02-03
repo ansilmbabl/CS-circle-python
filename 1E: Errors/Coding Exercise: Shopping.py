93d6656e5e8ee2c938e2a24123cdc51a9d33eabf
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-03 20:56:57 +0530

    Create Coding Exercise: Shopping.py

---
+++ 1E: Errors/Coding Exercise: Shopping.py
@@ -0,0 +1,14 @@
+"""
+Coding Exercise: Shopping
+
+You are going shopping for meat and milk, but there is tax. You buy $2.00 of milk and $4.00 of meat, and the tax rate is 3%. 
+Print out the total cost of your groceries (you don't need to print the dollar sign). Hint
+"""
+
+
+meatPrice = 4.00
+meatTax = 0.03 * meatPrice
+milkPrice = 2.00
+milkTax = 0.03 * milkPrice
+print(meatTax + meatPrice + milkTax + milkPrice)
+
