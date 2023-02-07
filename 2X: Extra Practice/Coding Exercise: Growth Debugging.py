fecb927c4fc7f8bbfc3289e4440fc092e204faf1
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-07 20:21:35 +0530

    Create Coding Exercise: Growth Debugging.py

---
+++ 2X: Extra Practice/Coding Exercise: Growth Debugging.py
@@ -0,0 +1,12 @@
+"""
+Coding Exercise: Growth Debugging
+  
+Fix the logic error in this program: it should calculate the population of your country for the next 3 years, assuming it starts with 1000 people in 2012, 
+and the number of people increases by 10% each year. You can change at most three characters.
+"""
+
+populationIn2012 = 1000
+populationIn2013 = populationIn2012 * 1.1
+populationIn2014 = populationIn2013 * 1.1
+populationIn2015 = populationIn2014 * 1.1
+
