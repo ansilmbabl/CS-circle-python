2a5c9754a8a7a62e4d5743a9209c8d043078d64d
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-22 21:15:46 +0530

    Create Coding Exercise: What's Your Sign?.py

---
+++ 6: If/Coding Exercise: What's Your Sign?.py
@@ -0,0 +1,13 @@
+"""
+Coding Exercise: What's Your Sign?
+Write a program that reads an integer from input, then outputs one of the capitalized words Positive, Negative, or Zero 
+according to whether the number is positive, negative, or zero.
+"""
+
+x=int(input())
+if x>0:
+ print("Positive")
+if x<0:
+ print("Negative")
+if x==0:
+ print("Zero")
