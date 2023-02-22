1938474fdc78afb06b6e7576110c8dc4c03b5309
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-22 21:17:53 +0530

    Create Coding Exercise: Age checker.py

---
+++ 6: If/Coding Exercise: Age checker.py
@@ -0,0 +1,17 @@
+"""
+Coding Exercise: Age checker
+
+Write a program that reads an integer from input, representing someone's age. If the age is 18 or larger, print out You can vote. 
+If the age is between 0 and 17 inclusive, print out Too young to vote. If the age is less than 0, print out You are a time traveller.
+"""
+
+age = int(input())
+if age >=18:
+ print("You can vote")
+if age <18:
+ if  age>=0:
+  print("Too young to vote")
+if age<0:
+  print("You are a time traveller")
+
+
