e39441ebdc94a5f4418f136f169cdffb3c08e1fe
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-03-15 19:13:06 +0530

    Create Coding Exercise: Skill-Testing Question.py

---
+++ 7B: Math/Coding Exercise: Skill-Testing Question.py
@@ -0,0 +1,11 @@
+"""
+Coding Exercise: Skill-Testing Question
+
+As a "skill-testing question", you are given three integers and asked to add the first two numbers, then multiply the result by the third number. 
+Write a program to read the three numbers and print out the correct answer. Hint
+"""
+n=int(input())
+m=int(input())
+k=int(input())
+
+print((n+m)*k)
