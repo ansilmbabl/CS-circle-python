76ac06aff655db4fc8da44d58b4455a81f7287fd
Ansil M B <86063895+ansilmbabl@users.noreply.github.com>
2023-04-03 16:59:14 +0530

    Create Coding Exercise: Rectangle.py

---
+++ 10: def/Coding Exercise: Rectangle.py
@@ -0,0 +1,8 @@
+"""
+Coding Exercise: Rectangle
+
+Define a function rectanglePerimeter(width, height) that returns the perimeter of a rectangle.
+"""
+
+def rectanglePerimeter(width, height):
+   return 2*(width+height)
