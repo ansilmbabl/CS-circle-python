3637e21e300359f95121a2b741380ad2a67859c2
Ansil M B <86063895+ansilmbabl@users.noreply.github.com>
2023-04-17 15:12:30 +0530

    Create Coding Exercise: Palindrome.py

---
+++ 13: Lists (Arrays)/Coding Exercise: Palindrome.py
@@ -0,0 +1,11 @@
+"""
+Coding Exercise: Palindrome
+
+A palindrome is a word which is spelled the same forwards as backwards. For example, the word
+"racecar"
+is a palindrome: the first and last letters are the same (r), the second and second-last letters are the same (a), etc. 
+Write a function isPalindrome(S) which takes a string S as input, and returns True if the string is a palindrome, and False otherwise.
+
+"""
+def isPalindrome(S):
+   return S == S[::-1]
