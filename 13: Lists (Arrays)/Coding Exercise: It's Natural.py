4208b8c6830c85f8530dce10f1b0551ea063aa24
Ansil M B <86063895+ansilmbabl@users.noreply.github.com>
2023-04-17 15:11:19 +0530

    Create Coding Exercise: It's Natural.py

---
+++ 13: Lists (Arrays)/Coding Exercise: It's Natural.py
@@ -0,0 +1,7 @@
+"""
+Coding Exercise: It's Natural
+Write a function naturalNumbers which takes a positive integer n as input, and returns a list [1, 2, ...] consisting of the first n natural numbers.
+"""
+
+def  naturalNumbers(n):
+   return [i for i in range(1,n+1)]
