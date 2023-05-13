175f2b9fc7396341f282753a2e843485eca5fddc
Ansil M B <86063895+ansilmbabl@users.noreply.github.com>
2023-05-13 11:12:55 +0530

    Update and rename Coding Exercise: 2D Distance.py to Coding Exercise: 2D Distance.md

--- 11C: Geometry/Coding Exercise: 2D Distance.py
+++
@@ -1,9 +0,0 @@
-"""
-Coding Exercise: 2D Distance
-
-Assume hypotenuse(a, b) has already been defined. Using it, define a function distance2D(x1, y1, x2, y2) which calculates the distance between the point (x1, y1) and 
-the point (x2, y2).
-"""
-
-def distance2D(x1, y1, x2, y2):
-   return hypotenuse(x1-x2, y1-y2)
